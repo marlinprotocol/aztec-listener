487c3d70a0689992aa1d6c548a179358aa03a9f0
akshay <akshay_111meher@hotmail.com>
2024-09-04 17:29:25 +0400

    multi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
@@ -5,12 +7,15 @@ async fn main() -> std::io::Result<()> {
     let port: u16 = 9003;
     let port_clone = port.clone().to_string();
 
+    let generator_address = env::var("GENERATOR").expect("GENERATOR is not set");
+    let gas_key = env::var("GAS_KEY").expect("GAS_KEY is not set");
+
     let listener =
     kalypso_listener::job_creator::JobCreator::simple_listener_for_non_confidential_prover(
-        "0x3A4980B3Fda8b1a15E2FA09963d681Ff14632C6f".into(),
+        generator_address,
         "1".into(),
         "http://88.99.141.248:8545".into(),
-        "0x33065ee43dde743065fbeb1753c1d50b20c09007b98ccbced40fe0c7ad166dec".into(),
+        gas_key,
         "0x6441dcD0f88f70E912A873baaeC5d02e564Ebc78".into(),
         "0x103e9C0e8E0A745A41F8A52142F452E7f8fAaCAd".into(),
         1,

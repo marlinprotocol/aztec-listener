c95f62c8894455b800808522b4e8cfb52ba3fc7d
akshay <akshay_111meher@hotmail.com>
2024-09-04 17:30:46 +0400

    update

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,20 @@ async fn main() -> std::io::Result<()> {
     let gas_key = env::var("GAS_KEY").expect("GAS_KEY is not set");
 
     let listener =
-    kalypso_listener::job_creator::JobCreator::simple_listener_for_non_confidential_prover(
-        generator_address,
-        "1".into(),
-        "http://88.99.141.248:8545".into(),
-        gas_key,
-        "0x6441dcD0f88f70E912A873baaeC5d02e564Ebc78".into(),
-        "0x103e9C0e8E0A745A41F8A52142F452E7f8fAaCAd".into(),
-        1,
-        987,
-        format!("http://localhost:{}/", port_clone),
-        format!("http://localhost:{}/", port_clone),
-        false,
-        2
-    );
+        kalypso_listener::job_creator::JobCreator::simple_listener_for_non_confidential_prover(
+            generator_address,
+            "1".into(),
+            "http://88.99.141.248:8545".into(),
+            gas_key,
+            "0x6441dcD0f88f70E912A873baaeC5d02e564Ebc78".into(),
+            "0x103e9C0e8E0A745A41F8A52142F452E7f8fAaCAd".into(),
+            1,
+            987,
+            format!("http://localhost:{}/", port_clone),
+            format!("http://localhost:{}/", port_clone),
+            false,
+            2,
+        );
 
     let _ = listener.run().await;
 
